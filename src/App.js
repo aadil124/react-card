9c003f785a94b0d27855d6d9573066b2be7e57bf
aadil124 <ansariaadil124@gmail.com>
2023-07-04 23:13:24 +0530

    removed static array data

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
 import CardData from "./components/CardData.jsx";
+import data from "./data/data.json";
 
 function App() {
-  const valueInCards = [
-    {
-      tier: "Free",
-      price: 0,
-      user: "Single User",
-      storage: "5GB Storage",
-      project: "Unlimited Public Projects",
-      access: "Community Access",
-      privateProjects: "Unlimited Private Projects",
-      phoneSupport: "Dedicated Phone Support",
-      subdomain: "Free Subdomain",
-      statusReport: "Monthly Status Reports",
-    },
-    {
-      tier: "Plus",
-      price: 9,
-      user: "5 User",
-      storage: "50GB Storage",
-      project: "Unlimited Public Projects",
-      access: "Community Access",
-      privateProjects: "Unlimited Private Projects",
-      phoneSupport: "Dedicated Phone Support",
-      subdomain: "Free Subdomain",
-      statusReport: "Monthly Status Reports",
-    },
-    {
-      tier: "Pro",
-      price: 49,
-      user: "Unlimited User",
-      storage: "150GB Storage",
-      project: "Unlimited Public Projects",
-      access: "Community Access",
-      privateProjects: "Unlimited Private Projects",
-      phoneSupport: "Dedicated Phone Support",
-      subdomain: "Free Subdomain",
-      statusReport: "Monthly Status Reports",
-    },
-  ];
   return (
     <>
-      <section class="pricing py-5">
-        <div class="container">
-          <div class="row">
-            {valueInCards.map((item) => {
-              return <CardData item={item} />;
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+            {data.map((item, index) => {
+              return <CardData item={item} key={index} />;
             })}
           </div>
         </div>
